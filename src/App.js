67916537b4dfb020ad4ca575d63bd1c7f9978e34
codefirst2022 <codefirst2022@gmail.com>
2023-12-16 15:26:50 +0530

    api code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Attendance from './components/Attendance';
+import Employee from './components/Employee';
+import SalaryAdvance from './components/SalaryAdvance';
+import SalaryVoucher from './components/SalaryVoucher';
+import Leaves from './components/Leaves';
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header/>
+       
+        <Routes>
+          <Route path='/' element={<Dashboard />} ></Route>
+          <Route path='/Attendance' element={<Attendance />} ></Route>
+          <Route path='/Employee' element={<Employee />} ></Route>
+          <Route path='/Salary-Advance' element={<SalaryAdvance />} ></Route>
+          <Route path='/Salary-Voucher' element={<SalaryVoucher />} ></Route>
+          <Route path='/Leaves' element={<Leaves />} ></Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
